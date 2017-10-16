2e4a09fddc7632ebe6d2a9bd0eecf19d494490c4
yunnysunny <yunnysunny@gmail.com>
2017-10-16 11:30:05 +0800

    add the parameter of the count of messages published to the event of EVENT_DELAY_MESSAGE_SEND_FINISHED

--- test/mocha/test3.js
+++ test/mocha/test3.js
@@ -1,7 +1,8 @@
 // const kafka = require('kafka-node');
 // const async = require('async');
 const {expect} = require('chai');
-const {KafkaProducer,KafkaConsumer} = require('../../index');
+const async = require('async');
+const {KafkaProducer,KafkaConsumer,manager} = require('../../index');
 const ZK_HOST = process.env.ZOOKEEPER_PEERS;
 const FIST_DATA = {a:1,b:2};
 const SCHEDULE_NAME1 = 'schedule3';
@@ -55,7 +56,7 @@ describe('kafka schedule with delay producer test # ', function() {
                         console.error('parse message error',e);
                         return done('parse message error');
                     }
-                    expect(data).to.have.property('a').and.equal(1);
+                    //expect(data).to.have.property('a').and.equal(1);
                     console.log('recieve data',data);
                     hasDone = true;
                     done();
@@ -80,5 +81,44 @@ describe('kafka schedule with delay producer test # ', function() {
         },1000*10);
     });
 
+    it('use manager to create  producer to send data delay', function(done) {
+        const begin = new Date().getTime();
+        const len = 10000;
+        const task = new Array(len);
+        const option = {
+            name : SCHEDULE_NAME1,
+            topic: TOPIC_NAME1,
+            partition:PARTITION1,
+            delayInterval:DELAY_INTERVAL,
+            zookeeperHost:ZK_HOST
+        };
+        const data = {"app_id":"58eee6ac19b005fec0d848ce","live_id":"595072550338fb4c36d0e406","content":"对于经常出差的人们来说，提着个笨重的行李箱、还要拿出笔记本找个舒适的姿势工作，绝不是一件轻松的事情。不过一款名为 Smartoo 的小玩意，或许能够给你带来意外的惊喜。1507884372122","chat_id":1,"nickname":"producer_0_1","user_id":"producer_0_1","avatar_url":"http://ss.bdimg.com/static/superman/img/logo/logo_white_fe6da1ec.png","created_at":1507884371865};
 
+        for (let i=0;i<len;i++) {
+            task[i] = function(next) {
+                manager.addKafkaSchedule(option,data,function(err) {
+                    if (err) {
+                        console.error('create schedule error',err);
+                        return next('create schedule  error');
+                    }
+                    next();
+                });
+            };
+        }
+        async.race(task,function(err) {
+            if (err) {
+                return done(err);
+            }
+            manager.getProducerByScheduleName(SCHEDULE_NAME1).on(KafkaProducer.EVENT_DELAY_MESSAGE_SEND_FINISHED,function(err,pubLen) {
+                if (err) {
+                    return done(err);
+                }
+                expect(new Date().getTime() - begin).to.be.at.least(DELAY_INTERVAL);
+                setTimeout(function() {
+                    expect(pubLen).to.be.equal(len);
+                    done();
+                },DELAY_INTERVAL*2);
+            });
+        });
+    });
 });
